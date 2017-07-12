60bb3d0c2fabd63db0b54bc7e0fb34c173717ee3
MrChengmo <cmchengmo@163.com>
2017-07-12 20:03:02 +0800

    手写数字识别程序

--- Tensorflow_MNIST/mnist_inference.py
+++ Tensorflow_MNIST/mnist_inference.py
@@ -20,4 +20,22 @@ def get_weight_variable(shape,regularizer):
     return(weights)
 
 #定义神经网络的前向传播过程
-    
+def inference(input_tensor,regularizer):
+    #声明第一层网络的变量并完成前向传播
+    with tf.variable_scope('layer1'):
+        weights = get_weight_variable(
+            [INPUT_NODE,LAYER1_NODE],regularizer)
+        biases = tf.get_variable(
+            "biases",[LAYER1_NODE],
+            initializer = tf.constant_initializer(0.0))
+        layer1 = tf.nn.relu(tf.matmul(input_tensor,weights)+biases)
+
+    #声明第二层网络的变量并完成前向传播
+    with tf.variable_scope('layer2'):
+        weights = get_weight_variable(
+            [INPUT_NODE,LAYER1_NODE],regularizer)
+        biases = tf.get_variable(
+            "biases",[LAYER1_NODE],
+            initializer = tf.constant_initializer(0.0))
+        layer2 = tf.nn.relu(tf.matmul(input_tensor,weights)+biases)
+    return(layer2)   
